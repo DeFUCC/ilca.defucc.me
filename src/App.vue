111b6594036685a66a00d9902e60eea26ace5e8b
davay <fond@fondfrukt.ru>
2022-10-16 19:47:56 +0700

    basic count

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,10 @@ import { RouterLink, RouterView } from 'vue-router'
 header.text-center.flex.flex-col.items-center
   .flex.flex-col.leading-none.gap-0.text-center.p-4
     .text-30vw.mx-auto.font-bold ILCA
-    .text-8vw.leading-tight International Letter Count and Account
-  .flex.gap-2.justify-center
-    RouterLink(to="/")
-      | Home
-    RouterLink(to="/about")
-      | About
+    .text-8vw.leading-tight International Letter 
+      RouterLink(to="/") Count 
+      span and 
+      RouterLink(to="/about") Account
 RouterView
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,40 @@
 <script setup>
-import { lettersToNum, numToLetters, colorize } from '../use/ilca';
+import { lettersToNum, numToLetters, colorize, cleanLetters } from '../use/ilca';
 
 const number = ref(0)
 const letters = ref('')
 
-watch(number, n => {
+
+function convertNL(n) {
 	letters.value = numToLetters(n)
+}
+
+function convertLN(l) {
+	number.value = lettersToNum(l)
+}
+
+watch(letters, l => {
+	letters.value = cleanLetters(l)
 })
 
-
+watch(number, n => {
+	if (n < 0)
+		number.value = 0
+})
 
 </script>
 
 <template lang="pug">
-main.p-2.md-p-4.flex.flex-col.gap-2
+main.p-2.md-p-4.flex.flex-col.gap-8.p-4.items-center
 	.text-xl Convert numbers and letters 
-	input.text-xl.font-bold.p-2(v-model="number" type="number")
-	input.text-xl.p-2(v-model="letters" :style="{backgroundColor: colorize(letters)}")
+	input.text-5xl.font-bold.p-2(
+		v-model.number="number"
+		type="number"
+		@input="convertNL($event.target.value)"
+		)
+	input.text-5xl.p-2(
+		v-model="letters" 
+		@input="convertLN($event.target.value)"
+		:style="{backgroundColor: colorize(letters)}"
+		)
 </template>
